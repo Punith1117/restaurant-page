0e452628fd81d1a5a28c8c93f343bcdb5546dc84
Punith1117 <punith11172006@gmail.com>
2024-09-26 12:55:57 +0530

    add header and js to display different tabs on click

--- src/index.js
+++ src/index.js
@@ -1,2 +1,44 @@
 import "./reset.css";
 import "./styles.css";
+import { homeContent } from "./home";
+import { menu } from "./menu";
+import { contact } from "./contact";
+let tabs = document.querySelectorAll('.tab');
+
+tabs.forEach((tab) => {
+    if(tab.id == "home") {
+        tab.setAttribute("data-on-this-tab", "true");
+        handleTab(tab);
+    } else {
+        tab.setAttribute("data-on-this-tab", "false");
+    }
+})
+
+tabs.forEach((tab) => {
+    tab.addEventListener('click', () => {
+        if(tab.dataset.onThisTab == 'false') {
+            removeOnThisTab();
+            tab.dataset.onThisTab = 'true';
+            handleTab(tab);
+        }
+    })
+})
+
+function removeOnThisTab() {
+    let tabs = document.querySelectorAll('.tab');
+    tabs.forEach((tab) => {
+        tab.dataset.onThisTab = 'false';
+    })
+}
+
+function handleTab(tab) {
+    let content = document.querySelector('#content');
+    content.innerHTML = ''
+    if (tab.id == 'home') {
+        content.appendChild(homeContent);
+    } else if (tab.id == 'menu') {
+        content.appendChild(menu);
+    } else {
+        content.appendChild(contact);
+    }
+}
